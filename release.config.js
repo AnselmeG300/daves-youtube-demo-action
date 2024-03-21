bc677b81c2bbf33772c774115e38a2888342cb14
Tchassem Boutchouang Anselme Gildas <aansegildass@yahoo.fr>
2024-03-21 11:27:25 +0100

    goe

--- release.config.js
+++ release.config.js
@@ -3,11 +3,12 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/git", {
-      "assets": ["dist/*.js", "dist/*.js.map"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    '@semantic-release/github'
+    [
+      "@semantic-release/github", {
+        "assets": ["dist/*.js", "dist/*.js.map"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
   ]
 };
 
